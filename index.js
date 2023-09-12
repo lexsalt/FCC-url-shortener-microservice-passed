22be252e7c1ddc9e0051b573e8906fcf09ffc986
emilianosal <emilianosal@hotmail.com>
2023-09-12 22:15:36 +0200

    passed test

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { MongoClient } = require('mongodb');
-let bodyparser = require("body-parser")
+let bodyParser = require("body-parser")
 const dns = require('dns')
-const urlparser = require('url')
+let mongoose = require('mongoose');
+//const urlparser = require('url')
 
-const client = new MongoClient(process.env.DB_URL)
-const db = client.db("urlshortner")
-const urls = db.collection("urls")
+
+// const client = new MongoClient(process.env.DB_URL)
+// const db = client.db("urlshortner")
+// const urls = db.collection("urls")
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -23,125 +27,95 @@ app.use(function middleware(req, res, next){
   next();
 })
 
+const URLSchema = new mongoose.Schema({
+  original_url: {type: String, required: true, unique: true},
+  short_url: {type: String, required: true, unique: true}
+});
+
+let URLModel = mongoose.model("url", URLSchema);
+
+// Middleware function to parse post requests
+app.use("/", bodyParser.urlencoded({ extended: false }));
+
+app.use(cors());
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.get('/api/shorturl/:short_url', function(req, res) {
+  let short_url = req.params.short_url;
+  // Find the original url from the database
+  URLModel.findOne({short_url: short_url}).then((foundURL) => {
+    if (foundURL){
+      let original_url = foundURL.original_url;
+      res.redirect(original_url);
+    } else {
+      res.json({message: "The short url does not exist!"});
+    }
+  });
+})
 
 // Your first API endpoint
 app.post('/api/shorturl', function(req, res) {
-  const url = req.body.url
-  console.log("url")
-  console.log(url)
-  // Validating the URL
+  let url = req.body.url;
+  // Validate the URL
   try {
-    urlObj = new URL(url)
-    console.log(urlObj)
-    dns.lookup(urlObj.hostname, async (err, address, family) => {
-      // if the dns domain doesnt exist no address will be returned
-      if (!address) {
-        console.log(`invalid url: ${address}`)
-        res.json({error: 'Invalid URL'})
-      } else { // means we have a valid URL
-        const urlCount = await urls.countDocuments({})
-        let original_url = urlObj.href
-        let storedUrl = urlObj.hostname
-        console.log(storedUrl)
-        let short_url = urlCount
-        const urlDoc = {
-          storedUrl,
-          short_url: urlCount
-        }
-  
-        const result = await urls.insertOne(urlDoc)
-        res.json({
-          original_url: original_url, short_url: short_url
+    urlObj = new URL(url);
+    // Validate DNS Domain
+    dns.lookup(urlObj.hostname, (err, address, family) => {
+      // If the DNS domain does not exist no address returned
+      if (!address){
+        res.json({ error: 'invalid url' })
+      } 
+      // We have a valid URL!
+      else {
+        let original_url = urlObj.href;
+        // Check that URL does not already exist in database 
+        URLModel.findOne({original_url: original_url}).then(
+          (foundURL) => {
+            if (foundURL) {
+              res.json(
+                {
+                  original_url: foundURL.original_url,
+                  short_url: foundURL.short_url
+                })
+          } 
+          // If URL does not exist create a new short url and 
+          // add it to the database
+          else {
+            let short_url = 1;
+            // Get the latest short_url
+            URLModel.find({}).sort(
+              {short_url: "desc"}).limit(1).then(
+                (latestURL) => {
+                if (latestURL.length > 0){
+                  // Increment the latest short url by adding 1
+                  short_url = parseInt(latestURL[0].short_url) + 1;
+                }
+                resObj = {
+                  original_url: original_url,
+                  short_url: short_url
+                }
+                
+                // Create an entry in the database
+                let newURL = new URLModel(resObj);
+                newURL.save();
+                res.json(resObj);
+                }
+            )
+          }
         })
       }
     })
   }
+  // If the url has an invalid format
   catch {
-    res.json({error: "invalid url"})
+    res.json({ error: 'invalid url' })
   }
-  //const parsed = urlparser.parse(url)
-  //console.log(parsed)
-  //console.log(parsed.pathname)
-  
-
 });
-
-/* 
-
-// Your first API endpoint
-app.post('/api/shorturl', function(req, res) {
-  console.log("body:")
-  console.log(req.body)
-  const url = req.body.url
-  console.log("url")
-  console.log(url)
-  const parsed = urlparser.parse(url)
-  //console.log(parsed)
-  console.log(parsed.pathname)
-  
-  const dnslookup = dns.lookup(parsed.pathname, async (err, address) => {
-    if (!address){
-      res.json({error: "Invalid URL"})
-    } else {
-
-      const urlCount = await urls.countDocuments({})
-      const urlDoc = {
-        url,
-        short_url: urlCount
-      }
-
-      const result = await urls.insertOne(urlDoc)
-      console.log(result);
-      res.json({ original_url: url, short_url: urlCount })
-      
-    }
-  })
-});
-
-*/
-
-
-// app.get("/api/short_url/:short_url", async (req, res) => {
-//   console.log("underScore")
-//   const shorturl1 = req.params.short_url
-//   console.log(`params: ${req.params.short_url}`)
-//   const urlDoc = await urls.findOne({ short_url: +shorturl1 })
-//   console.log(`params: ${urlDoc.url}`)
-//   res.redirect(urlDoc.storedUrl)
-// })
-
-app.get("/api/shorturl/:shorturl", async (req, res) => {
-  console.log("no underScore")
-  const shorturl1 = req.params.shorturl
-  console.log(`params: ${req.params.shorturl}`)
-  const urlDoc = await urls.findOne({ short_url: +shorturl1 })
-  console.log(`type: ${typeof(urlDoc)}`)
-  if (urlDoc !== "null") {
-    console.log(" no es nulo")
-  }
-  // need to catch errors
-  console.log(`urlDoc: ${urlDoc}`)
-  console.log(urlDoc)
-  console.log(urlDoc.storedUrl)
-  console.log(`params: ${urlDoc.url}`)
-  res.redirect(`https://${urlDoc.storedUrl}`)
-  
-  
-})
-
-// app.get("/api/shorturl/undefined", async (req, res) => {
-//   console.log("undefined")
-//   res.json({
-//     error: "Wrong format"
-//   })
-// })
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
